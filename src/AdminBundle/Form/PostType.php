a8e335eced084ba8ad6363acf6739e2c54fba0b5
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2017-08-06 18:28:11 +0100

    Gestion des post part 1 et 2

---
+++ src/AdminBundle/Form/PostType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class PostType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title')
+                ->add('description')
+                ->add('body')
+                ->add('slug')
+                ->add('datepublish', DateType::class, array(
+                       'widget' => 'single_text',
+                       'format' => 'yyyy-MM-dd',
+                       'data'   => new \DateTime()
+                    ));
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AdminBundle\Entity\Post'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'adminbundle_post';
+    }
+
+
+}
