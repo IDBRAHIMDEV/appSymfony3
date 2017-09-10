352c67c38ebf400114069a98e5435c4d85b230cf
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2017-09-10 15:13:45 +0100

    Ajout de la nouvelle page blog

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,11 +17,44 @@ class DefaultController extends Controller
 
         $em = $this->getDoctrine()->getManager();
         $posts = $em->getRepository('AdminBundle:Post')->findBy([], ['datepublish' => 'desc'], 3, 0);
+
         return $this->render('default/index.html.twig', [
             'posts' => $posts,
         ]);
     }
 
+    
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function blog() {
 
-   
+        $em = $this->getDoctrine()->getManager();
+        $posts = $em->getRepository('AdminBundle:Post')->findAll();
+
+        return $this->render('default/blog.html.twig', [
+            'posts' => $posts,
+        ]);
+
+ 
+    }
+
+
+
+        /**
+     * @Route("/blog/{id}", name="blog-show")
+     */
+    public function show($id) {
+
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository('AdminBundle:Post')->find($id);
+
+        return $this->render('default/show.html.twig', [
+            'post' => $post,
+        ]);
+
+ 
+    }
+
+
 }
